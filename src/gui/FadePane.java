ce064c010712717db220dd5011a9193db238f84f
wokobo <kalazeus@gmail.com>
2023-03-27 20:27:38 +0200

    gui fix

--- src/gui/FadePane.java
+++ src/gui/FadePane.java
@@ -3,31 +3,75 @@ package gui;
 import application.Lager;
 import controller.Controller;
 import javafx.geometry.Insets;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 public class FadePane extends GridPane {
     private Controller controller = Controller.getController();
     private ListView<Lager> lstLager = new ListView<>();
-    private Button btnOpretLager = new Button("Opret lager");
-    private Label antalLagre = new Label("Antal lagre: " + controller.getAlleLagre().size());
-    private Label totalAntalFad = new Label("Total antal fad: " + controller.totalAntalFad());
+    private Label lblNuværendeLager = new Label("Lager Liste:");
+    private Label lblLokation = new Label("Lokation");
+    private Label lblPladser = new Label("Pladser");
+    private Label lblError = new Label();
+    private TextField txtFadStr = new TextField();
+    private TextField txtFadType = new TextField();
+
+
     public FadePane() {
 
         this.setPadding(new Insets(20));
         this.setHgap(20);
         this.setVgap(10);
         this.setGridLinesVisible(false);
-        //----------------------------------------------
 
-
-        this.add(lstLager, 0, 0);
+        //----------------------------------------------list
+        this.add(lstLager, 0, 1);
         this.lstLager.setEditable(false);
         this.lstLager.getItems().setAll(controller.getAlleLagre());
-        this.add(btnOpretLager, 15, 2);
-//        btnOpretLager.setOnAction(event -> actionopretLager());
-        this.add(antalLagre, 0, 1);
-        this.add(totalAntalFad, 0 , 2);
-}}
+        this.lstLager.setMinHeight(200);
+
+
+
+        //------------------------------------------------- Labels
+
+        this.add(lblNuværendeLager, 0, 0);
+
+
+        //------------------------------------------------button
+
+        //-------------------------------------------------text
+
+
+        //-------------------------------------------------
+
+
+
+
+
+
+    }
+
+
+
+    public void updateControls() {
+        this.lstLager.getItems().setAll(controller.getAlleLagre());
+
+
+
+
+    }
+    private void btnOpretAction() {
+        String typeFad = txtFadType.getText().trim();
+        double fadStr = Double.parseDouble(txtFadStr.getText().trim());
+        if (typeFad.length() == 0 || fadStr == 0 || txtFadStr.getText().trim().length() == 0) {
+            lblError.setText("Udfyld venligst alle felter");
+        } else {
+            //TODO: noget med en listener
+            controller.opretFad(typeFad,fadStr,controller.getLagerById(1) );
+            this.updateControls();
+        }
+    }
+
+}
