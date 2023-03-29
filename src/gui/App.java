b15967d0b61c8c4b9de10bb1263e2af39baa4b22
wokobo <kalazeus@gmail.com>
2023-03-29 21:53:57 +0200

    gui FadePane v1.0

--- src/gui/App.java
+++ src/gui/App.java
@@ -13,7 +13,7 @@ public class App {
     private static Controller controller = Controller.getController();
 
     public static void main(String[] args) {
-        //Application.launch(StartWindow.class);
+
         Lager l1 = controller.opretLager("Aarhus",100);
         Fad f1 = controller.opretFad("Grim",50.0, l1);
         LagretVæske lV1 = controller.opretLagretVæske(30, LocalDate.now());
@@ -21,7 +21,7 @@ public class App {
 
         controller.fyldPåSpecifiktFad(30,LocalDate.now(),f1,d1, lV1);
         System.out.println(lV1.getFadehistorik());
-
+        Application.launch(StartWindow.class);
 
 
 
